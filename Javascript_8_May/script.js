3685e856648a5feb28d94b3938ab50cfd4b2e918
harshit391 <harshitsingla1761@gmail.com>
2023-05-08 11:51:07 +0530

    Javascript_8_May

---
+++ Javascript_8_May/script.js
@@ -0,0 +1,81 @@
+// //object
+
+// let person = 
+// {
+//     name: "Harshit",
+//     age:24,
+//     place:"Sanaur",
+
+//     //declaring a function inside object
+
+//     giveintro:function()
+//     {
+//         console.log(`My name is ${this.name}`)
+//     }
+
+//     //this signify the current object properties
+// }
+
+// person.giveintro();
+// //calling the function inside the person object
+
+// console.log(person)
+
+// person["state"] = "Punjab"; 
+// //Adding a new entry in the object
+
+// console.log(person)
+
+// delete person["state"];
+// console.log(person)
+
+// ===============================================================================================================================
+
+
+// // date object
+
+// let date = new Date();
+// //This will show current date time timezone returned by our system data
+
+// console.log(date)
+
+
+// console.log(date.toDateString()) 
+// //This will show only Day and date
+
+
+// console.log(date.getDate())
+// // THis will show only current date only date number
+
+// console.log(date.getDay())
+// // This will show current weekday
+
+// console.log(date.getFullYear())
+// // This will show current Year
+
+// date.setMonth(8)
+// // It will manipulate the month to our wish only for browser not for system
+
+// console.log(date.getMonth())
+
+
+// date.setFullYear(2022)
+// console.log(date.getDay())
+
+// ===============================================================================================================================
+
+
+// let date = new Date('23 march, 1999')
+// console.log(date)
+
+// // This will show the date we want to show in browser
+
+// ================================================================================================================================
+
+
+// Selectors
+
+// textContent, innerHTML, innerText
+
+
+
